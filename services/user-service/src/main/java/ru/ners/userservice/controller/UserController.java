087c1ac372e9fb8cb19efe2b7b8a16cdb0f9e48d
Artem <artemgun2004@gmail.com>
2024-11-09 23:33:12 +0300

    create controller

--- services/user-service/src/main/java/ru/ners/userservice/controller/UserController.java
+++ services/user-service/src/main/java/ru/ners/userservice/controller/UserController.java
@@ -3,29 +3,59 @@ package ru.ners.userservice.controller;
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import net.devh.boot.grpc.server.service.GrpcService;
 import ru.ners.service.UserManagement;
 import ru.ners.service.UserServiceGrpc;
 import ru.ners.userservice.model.User;
 import ru.ners.userservice.service.UserService;
 
-@Controller
+@GrpcService
 @RequiredArgsConstructor
 public class UserController extends UserServiceGrpc.UserServiceImplBase {
     private final UserService userService;
 
     @Override
-    public void saveUser(UserManagement.UserRequest request, StreamObserver<Empty> responseObserver) {
-        
+    public void saveUser(UserManagement.SaveUserRequest request, StreamObserver<Empty> responseObserver) {
+        var user = User.builder()
+                .username(request.getUsername())
+                .passwordHash(request.getPassword())
+                .contacts(request.getContactsMap())
+                .build();
+
+        userService.saveUser(user);
     }
 
     @Override
-    public void getUser(UserManagement.UserRequest request, StreamObserver<UserManagement.UserResponse> responseObserver) {
-        super.getUser(request, responseObserver);
+    public void getUser(UserManagement.GetUserRequest request, StreamObserver<UserManagement.GetUserResponse> responseObserver) {
+        var user = userService.getUser(request.getId());
+
+        var response = UserManagement.GetUserResponse.newBuilder()
+                .setId(user.getId())
+                .setUsername(user.getUsername())
+                .setPasswordHash(user.getPasswordHash())
+                .putAllContacts(user.getContacts())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
-    public void getUsers(Empty request, StreamObserver<UserManagement.UsersResponse> responseObserver) {
-        super.getUsers(request, responseObserver);
+    public void getUsers(Empty request, StreamObserver<UserManagement.GetUsersResponse> responseObserver) {
+        var users = userService.getUsers();
+
+        var responseBuilder = UserManagement.GetUsersResponse.newBuilder();
+        users.forEach(user -> responseBuilder.addUsers(
+                UserManagement.GetUserResponse.newBuilder()
+                        .setId(user.getId())
+                        .setUsername(user.getUsername())
+                        .setPasswordHash(user.getPasswordHash())
+                        .putAllContacts(user.getContacts())
+                        .build()
+                )
+        );
+
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
     }
 }
